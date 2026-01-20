75c227cb13b59a1a2d7a741988a705b7993e6b16
Jasper Garcia <garciajaspermercado24@gmail.com>
2026-01-20 18:55:04 +0800

    updates in navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,10 +9,7 @@ import NavbarItem from "./NavBarItem";
 
 const Navigation = () => {
   return (
-    <div className="relative flex flex-row grow shrink max-w-[50%] xl:max-w-[30%] content-center h-13 rounded-full overflow-hidden px-1 py-1 bg-accents shadow-[4px_4px_4px_rgba(0,0,0,0.25)]">
-      <div
-        className={`absolute top-1 bottom-1 bg-secondary rounded-l-full z-0 ${location.hash === "#home" ? "left-1 right-98" : ""}`}
-      />
+    <div className="relative flex flex-row grow shrink max-w-[50%] xl:max-w-[30%] content-center max-h-13 rounded-full overflow-hidden px-1 py-1 bg-accents shadow-[4px_4px_4px_rgba(0,0,0,0.25)]">
       <NavbarItem label="Home" to="#home" position="left" icon={faHome} />
       <NavbarItem label="Projects" to="#projects" icon={faBriefcase} />
       <NavbarItem label="Skills" to="#skills" icon={faFileCode} />
