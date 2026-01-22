a4787c56571cd83fb3d6dc376c16c1c8ad032c8f
Jasper Garcia <garciajaspermercado24@gmail.com>
2026-01-22 14:09:14 +0800

    updated socials

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,8 +1,8 @@
 import {
-  faFacebook,
   faInstagram,
   faLinkedin,
   faUpwork,
+  faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
@@ -10,10 +10,12 @@ import { Link } from "react-router-dom";
 const Socials = () => {
   const links = {
     upwork: "https://www.upwork.com/freelancers/~0177c2ceaba26a100f",
-    linkedin: "https://www.linkedin.com/in/jasper-garcia-a0b8963a6/",
+    linkedin: "https://www.linkedin.com/in/jasper-garcia-a0b8963a6",
+    youtube: "https://www.youtube.com/@jasper-garcia",
+    instagram: "https://www.instagram.com/garciajaspermercado24",
   };
   return (
-    <div className="flex flex-row w-[100%] items-center justify-center gap-10 h-13 max-w-[50%] xl:max-w-[30%] rounded-full overflow-hidden px-1 py-1">
+    <div className="xl:max-w-[30%] flex flex-row gap-10 flex-wrap items-center justify-center">
       <Link to={links.upwork} target="_blank">
         <FontAwesomeIcon className="text-secondary" icon={faUpwork} size="lg" />
       </Link>
@@ -24,12 +26,20 @@ const Socials = () => {
           size="lg"
         />
       </Link>
-      <FontAwesomeIcon
-        className="text-secondary"
-        icon={faInstagram}
-        size="lg"
-      />
-      <FontAwesomeIcon className="text-secondary" icon={faFacebook} size="lg" />
+      <Link to={links.youtube} target="_blank">
+        <FontAwesomeIcon
+          className="text-secondary"
+          icon={faYoutube}
+          size="lg"
+        />
+      </Link>
+      <Link to={links.instagram} target="_blank">
+        <FontAwesomeIcon
+          className="text-secondary"
+          icon={faInstagram}
+          size="lg"
+        />
+      </Link>
     </div>
   );
 };
